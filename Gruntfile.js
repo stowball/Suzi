ad74bab6ec2594e253764784bf1aed963f8dc696
Matt Stow <matt@mattstow.com>
2014-10-16 08:24:07 +1100

    Move fastclick.js up in the script order as it will always be used

--- Gruntfile.js
+++ Gruntfile.js
@@ -98,8 +98,8 @@ module.exports = function (grunt) {
 					'<%= globalConfig.path.js.vendor %>/enquire.js',
 					'<%= globalConfig.path.js.vendor %>/rwd.images.js',
 					'<%= globalConfig.path.js.vendor %>/yepnope.js',
-					'<%= globalConfig.path.js.vendor %>/swipe.js',
 					'<%= globalConfig.path.js.vendor %>/fastclick.js',
+					'<%= globalConfig.path.js.vendor %>/swipe.js',
 					'<%= globalConfig.path.js.vendor %>/jquery.transit.js',
 					'<%= globalConfig.path.js.src %>/all.js'
 				],
