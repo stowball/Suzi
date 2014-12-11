5387acb552261b8ca52d6ae1512fd47b95c29dd0
Matt Stow <matt@mattstow.com>
2014-12-11 11:11:50 +1100

    Made placeholder polyfill smaller & faster

--- js/all.js
+++ js/all.js
@@ -86,23 +86,24 @@ var grid = {
 var placeholder = {
 	init: function() {
 		if (!Modernizr.placeholder) {
-			var $placeholder = $('[placeholder]');
-			
-			$placeholder.focus(function() {
-				var $input = $(this);
-				if ($input.val() === $input.attr('placeholder'))
-					$input.val('').removeClass('placeholder');
-			}).blur(function() {
-				var $input = $(this);
-				if ($input.val() === '' || $input.val() === $input.attr('placeholder'))
-					$input.addClass('placeholder').val($input.attr('placeholder'));
-			}).blur();
-			
-			$placeholder.parents('form').on('submit', function() {
-				$(this).find('[placeholder]').each(function() {
-					var $input = $(this);
-					if ($input.val() === $input.attr('placeholder'))
-						$input.val('');
+			$('[placeholder]').each(function() {
+				var $this = $(this),
+					attr = this.getAttribute('placeholder');
+				
+				$this.focus(function() {
+					if (this.value === attr) {
+						$this.removeClass('placeholder')[0].value = '';
+					}
+				}).blur(function() {
+					if (this.value.length === 0 || this.value === attr) {
+						$this.addClass('placeholder')[0].value = attr;
+					}
+				}).blur();
+				
+				$this.parents('form').on('submit', function() {
+					if ($this[0].value === attr) {
+						$this[0].value = '';
+					}
 				});
 			});
 			
